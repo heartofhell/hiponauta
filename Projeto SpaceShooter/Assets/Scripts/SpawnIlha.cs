49c4ede74f54f4b2cdc0fea1d26fd48a1db7a087
icaro <icaro_martins98@hotmail.com>
2017-10-13 10:13:54 -0300

    13/10/2017

---
+++ Projeto SpaceShooter/Assets/Scripts/SpawnIlha.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnIlha : MonoBehaviour {
+
+	public GameObject ilha;
+	public GameObject posicao;
+	public float delay;
+	private bool spawn;
+
+	// Use this for initialization
+	void Start () {
+		delay = 1;
+		spawn = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(spawn == true){
+			Instantiate (ilha, posicao.transform.position, Quaternion.identity);
+			spawn = false;
+		}
+	}
+}
---
+++ Projeto SpaceShooter/Assets/Scripts/actionIlha.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class actionIlha : MonoBehaviour {
+
+	public Rigidbody2D corpo;
+	public float velocidade;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		corpo.AddForce (new Vector2(0, -velocidade));
+	}
+}
